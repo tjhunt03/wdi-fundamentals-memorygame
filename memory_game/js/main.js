9ced28d293897dc131a1cb63f0188762e6835430
Thomas Hunt <tjhunt03@gmail.com>
2018-08-28 19:20:16 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,31 @@
 
-console.log("Up and running!");
+var cards = ['queen', 'queen', 'king', 'king']
+
+var cardsInPlay = [];
+
+var card0ne = cards [0];
+cardsInPlay.push(card0ne);
+console.log ("user flipped " + card0ne);
+
+var cardtwo = cards [2];
+cardsInPlay.push(cardtwo);
+console.log ("user flipped " + cardtwo);
+
+if 
+(cardsInPlay.length === 2) {
 
 
-var card0ne =  "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+}
+
+if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("you found a match");
+ } else {
+
+	alert("Sorry, try again");
+
+}
 
 
-console.log("User flipped " + card0ne);
-console.log("User flipped " + cardTwo);
+
+
+
