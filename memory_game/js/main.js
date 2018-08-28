b39476014cd47587c9ec79711f334361168127f0
Thomas Hunt <tjhunt03@gmail.com>
2018-08-28 11:03:24 +1000

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,12 @@
+
+console.log("Up and running!");
+
+
+var card0ne =  "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+
+console.log("User flipped " + card0ne);
+console.log("User flipped " + cardTwo);
